5bb6fb6383019c64c04729752393da103bf31a6d
Ponwat <buddykung9000@gmail.com>
2024-12-05 23:04:06 +0700

    commit 1

--- login.php
+++ login.php
@@ -23,7 +23,7 @@
             padding: 30px 40px;
             border-radius: 12px;
             box-shadow: 0 8px 20px rgba(0, 0, 0, 0.2);
-            width: 350px;
+            width: 500px;
         }
         h1 {
             font-size: 24px;
@@ -42,6 +42,9 @@
         .back-link:hover {
             text-decoration: underline;
         }
+        .image {
+            height: 200px;
+        }
     </style>
 </head>
 <body>
@@ -58,10 +61,16 @@
             if ($username === $valid_username && $password === $valid_password) {
                 echo "<h1 style='color: green;'>Login Successful</h1>";
                 echo "<p>Welcome, " . htmlspecialchars($username) . "!</p>";
+                echo "<h5> TVpXR0NaMzNKRlhWSVJLN0pSUVdJMjNTTUZSR0MzVEhQVT09PT09PQ== </h5>";
+                echo "<p>Send the result</p>";
+                echo "<img class=\"image\" src=\"qr-code.png\" alt=\"qr-code\"><br>";
+                echo "<a href=\"https://forms.gle/mKDMi4SDyYweiaRW6\">https://forms.gle/mKDMi4SDyYweiaRW6</a>";
             } else {
                 echo "<h1 style='color: red;'>Login Failed</h1>";
                 echo "<p>Invalid username or password.</p>";
             }
+        } else {
+            header('Location: index.html');
         }
         ?>
         <a href="index.html" class="back-link">Go back to login</a>
--- reset-password.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = isset($_POST['email']) ? $_POST['email'] : '';
-    echo "<h1>Password Reset</h1>";
-    echo "<p>An email has been sent to " . htmlspecialchars($email) . " with reset instructions.</p>";
-}
-?>
